613092e26bba955c2f2f2a59461cbf3a72f0d4bf
monty-jose <monti.jose04@gmail.com>
2021-03-17 07:27:26 -0300

    Primer commit -  Inicio de Proyecto

---
+++ QuasarFireOperation/Models/Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuasarFireOperation.Models
+{
+    public class Position
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+    }
---
+++ QuasarFireOperation/Models/Satellite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuasarFireOperation.Models
+{
+    public class Satellite
+    {
+        public string name { get; set; }
+        public double distance { get; set; }
+        public string [] message { get; set; }
+        public Position position { get; set; }
+    }
+}
